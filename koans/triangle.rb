f811d214c217183bec15db5c536217044d34d98c
Eugene V <kokokoi9@mail.ru>
2017-04-15 17:23:03 +0300

    fixed about_triangle_project_2.rb

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  a, b, c = [a, b, c].sort
+  raise TriangleError if a <= 0 || a + b <= c
+
   if a == b && b == c
   	:equilateral
   elsif a == b || b == c || c == a
